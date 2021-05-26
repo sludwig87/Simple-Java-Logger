ee8e2100f6f58bad16d6bedb08020632ce0b5d0b
Stephen Ludwig <stephenmludwig87@gmail.com>
2021-05-25 22:11:18 -0400

    - Converted Simple Logger to printf statements to make life easier. -Added some log levels.

--- src/simplejavalogger/SimpleLogger.java
+++ src/simplejavalogger/SimpleLogger.java
@@ -17,13 +17,28 @@ import java.time.LocalDateTime;
  * @author Malice
  *
  */
+
+
 public class SimpleLogger 
 {
+
+	/// Private members
+	private static String logFile = "demo_log.txt";
+	private PrintWriter writer;  
+	private DateTimeFormatter dateTimeFormat = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 	
-	/// Singleton Pattern
+	public enum LogLevel
+	{
+		TRACE, //Lowest Level
+		DEBUG,
+		INFO,
+		ERROR
+	}
+	
+	static private LogLevel logLevel = LogLevel.INFO;
+	
+	/// ==== Singleton Pattern ====
 	private static SimpleLogger _logger = null;
-	
-	
 	public static synchronized SimpleLogger getInstance()
 	{
 	    if(_logger == null)
@@ -34,33 +49,55 @@ public class SimpleLogger
 	    return _logger;
 	}
 	
-	public static synchronized SimpleLogger getInstance(String nFileName)
+	
+	
+	public static synchronized SimpleLogger getInstance(String newFileName)
 	{
 	    if(_logger == null)
 	    {
-	    	if(nFileName.length() > 0)
+	    	if(newFileName.length() > 0)
 	    	{
-	    		logFile = nFileName;
+	    		logFile = newFileName;
 	    	}
 	        _logger = new SimpleLogger();
 	    }
-	    else if(logFile != nFileName)
+	    else if(logFile != newFileName)
 	    {
-	    	System.out.printf("Error In SimpleLogger. Log File Already Specified Elsewhere. Cannot Set log file to %s", nFileName);
+	    	System.out.printf("Error In SimpleLogger. Log File Already Specified Elsewhere. Cannot Set log file to %s", newFileName);
 	    }
 	    
 	    return _logger;
 	}
 	
-	/// Private members
-	private static String logFile = "demo_log.txt";
+	public static synchronized SimpleLogger getInstance(String newFileName, LogLevel newLogLevel)
+	{
+	    if(_logger == null)
+	    {
+	    	if(newFileName.length() > 0)
+	    	{
+	    		logFile = newFileName;
+	    	}
+	    	
+	    	logLevel = newLogLevel; 
+	    	
+	    	
+	        _logger = new SimpleLogger();
+	    }
+	    else if(logFile != newFileName)
+	    {
+	    	System.out.printf("Error In SimpleLogger. Log File Already Specified Elsewhere. Cannot Set log file to %s", newFileName);
+	    }
+	    
+	    return _logger;
+	}
+	
+	///==========================
 	
 	
-    private PrintWriter writer;  
-    private DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+	
     
-    
-    // Private Methods
+    ///     Private Methods
+	/// Basic Constructor
     private SimpleLogger() 
     {
         try 
@@ -73,51 +110,90 @@ public class SimpleLogger
         
 	} 
     
-    // Public Methods
+    
+    /// Basic Log Function used in more specific log functions
     public void Log(String formatString, Object ...args)
     {
-    	String formatedString = formatString;
-    	
-    	// Basic Python replace loop
-    	// TODO: Need to handle the {1}... etc cases
-    	for(Object toReplace : args) 
+    	// Attempt to just pass the inputs to the printf function of
+    	// the print writer class.
+    	try 
     	{
-    		try 
-    		{
-    			formatedString = formatedString.replaceFirst("\\{}", toReplace.toString());
-    		}
-    		catch(Exception e)
-    		{
-    			writer.printf("Failed to log message: %s", formatString);
-    		}
+    		writer.printf(formatString, args);
+    	}
+    	catch (Exception logError)
+    	{
+    		writer.printf("Failed to format input: %s", formatString);
     	}
     	
-    	writer.println(formatedString);
     }
     
-    
-    
+      
     /// Public Formated Log Messages
     public void Error(String formatString, Object ...args)
     {
-    	this.Log(dtf.format(LocalDateTime.now()) + "[Error] - " + formatString, args);
+    	this.Log(dateTimeFormat.format(LocalDateTime.now()) + "[Error] - " + formatString, args);
     }
     
     public void Trace(String formatString, Object ...args)
     {
-    	this.Log(dtf.format(LocalDateTime.now()) + "[Trace] - " + formatString, args);
+    	this.Log(dateTimeFormat.format(LocalDateTime.now()) + "[Trace] - " + formatString, args);
     }
     
     public void Debug(String formatString, Object ...args)
     {
     	
-    	this.Log(dtf.format(LocalDateTime.now()) + "[Debug] - " + formatString, args);
+    	this.Log(dateTimeFormat.format(LocalDateTime.now()) + "[Debug] - " + formatString, args);
     }
     
     public void Info(String formatString, Object ...args)
     {
     	
-    	this.Log(dtf.format(LocalDateTime.now()) + "[Info] - " + formatString, args);
+    	this.Log(dateTimeFormat.format(LocalDateTime.now()) + "[Info] - " + formatString, args);
+    }
+    
+    
+    
+    public Boolean SetLogLevel(LogLevel newLogLevel)
+    {
+    	try 
+    	{
+    		logLevel = newLogLevel;
+    	}
+    	catch(Exception e)
+    	{
+    		//TODO: Make this better
+    		return false;
+    	}
+    	
+    	return true;
+
+    }
+    
+    
+    public Boolean SetLogLevel(String newLogLevelString)
+    {
+    	if( newLogLevelString.contains("TRACE") )
+    	{
+    		logLevel = LogLevel.TRACE;
+    	}
+    	else if(newLogLevelString.contains("ERROR") )
+    	{
+    		logLevel = LogLevel.ERROR;
+    	}
+    	else if(newLogLevelString.contains("DEBUG") )
+    	{
+    		logLevel = LogLevel.DEBUG;
+    	}
+    	else if(newLogLevelString.contains("INFO") )
+    	{
+    		logLevel = LogLevel.INFO;
+    	}
+    	else
+    	{
+    		return false;
+    	}
+    	
+    	return true;
     }
     
 }
